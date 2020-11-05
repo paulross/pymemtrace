a1454ec95f2bdfeb7c0fd178936b902fd3dafa3b
Paul Ross <apaulross@gmail.com>
2020-11-05 11:46:48 +0000

    Fix and test stdout re-director.

--- tests/test_redirect_stdout.py
+++ tests/test_redirect_stdout.py
@@ -7,13 +7,57 @@ import pytest
 from pymemtrace import redirect_stdout
 
 
-def test_stdout_redirector_python():
-    stream = io.StringIO()
+def test_stdout_redirector_python_bytes():
+    stream = io.BytesIO()
     with redirect_stdout.stdout_redirector(stream):
         sys.stdout.write('Foo')
     result = stream.getvalue()
-    assert result == 'Foo'
+    assert result == b'Foo', f'Result: {result!r}'
+
+
+# def test_stdout_redirector_python_text():
+#     stream = io.StringIO()
+#     with redirect_stdout.stdout_redirector(stream):
+#         sys.stdout.write('Foo')
+#     result = stream.getvalue()
+#     assert result == b'Foo', f'Result: {result!r}'
+
+
+def test_stdout_redirector_c_bytes():
+    libc = ctypes.CDLL(None)
+    stream = io.BytesIO()
+    with redirect_stdout.stdout_redirector(stream):
+        libc.puts(b'This comes from C')
+    result = stream.getvalue()
+    assert result == b'This comes from C\n', f'Result: {result!r}'
+
+
+def test_stdout_redirector_python_c_bytes():
+    libc = ctypes.CDLL(None)
+    stream = io.BytesIO()
+    with redirect_stdout.stdout_redirector(stream):
+        print('foobar')
+        sys.stdout.write('sys.stdout.write()\n')
+        libc.puts(b'This comes from C')
+    result = stream.getvalue()
+    assert result == b'This comes from C\nfoobar\nsys.stdout.write()\n', f'Result: {result!r}'
+
+
+def test_stdout_redirector_python_c_bytes_flush():
+    libc = ctypes.CDLL(None)
+    stream = io.BytesIO()
+    with redirect_stdout.stdout_redirector(stream):
+        print('foobar')
+        sys.stdout.write('sys.stdout.write()\n')
+        sys.stdout.flush()
+        libc.puts(b'This comes from C')
+    result = stream.getvalue()
+    assert result == b'foobar\nsys.stdout.write()\nThis comes from C\n', f'Result: {result!r}'
 
 
 if __name__ == '__main__':
-    test_stdout_redirector_python()
+    test_stdout_redirector_python_bytes()
+    # test_stdout_redirector_python_text()
+    test_stdout_redirector_c_bytes()
+    test_stdout_redirector_python_c_bytes()
+    test_stdout_redirector_python_c_bytes_flush()
