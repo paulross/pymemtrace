7a51446afb231c325b406af5eb625827746f55c4
Paul Ross <apaulross@gmail.com>
2024-11-04 13:41:09 +0000

    Add initial Python tests for cPyMemTrace.

---
+++ tests/test_cpymemtrace.py
@@ -0,0 +1,13 @@
+"""
+At the moment these produce a log file per test.
+TODO: Would be handy if we could specify a temporary file or file path, inspect it and clean up.
+"""
+
+import pytest
+
+from pymemtrace import cPyMemTrace
+
+
+def test_basic():
+    with cPyMemTrace.Profile(0):
+        b' ' * (1024 ** 2)
