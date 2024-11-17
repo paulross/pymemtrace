7dd4f07c3ff36866e8b3779d9787903e07895152
Paul Ross <apaulross@gmail.com>
2024-11-17 19:10:01 +0000

    Commit version to 0.2.0.

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ else:
 
 setup(
     name='pymemtrace',
-    version='0.2.0rc0',
+    version='0.2.0',
     description="Python memory tracing.",
     long_description=readme + '\n\n' + history,
     long_description_content_type='text/x-rst',
