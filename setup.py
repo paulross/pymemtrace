bb78e05b11da42c9495cb7a9ffb47f8d5d6417ed
Paul Ross <apaulross@gmail.com>
2026-07-27 22:25:36 +0100

    Bump version to 0.6.3rc0.

--- setup.py
+++ setup.py
@@ -122,7 +122,7 @@ if sys.version_info.major >= 3 and sys.version_info.minor >= 13:
 
 setup(
     name='pymemtrace',
-    version='0.6.2',
+    version='0.6.3rc0',
     description="Python memory tracing.",
     long_description=readme + '\n\n' + history,
     long_description_content_type='text/x-rst',
